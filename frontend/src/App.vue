7b2912596ba0a50056c5f865c0bf712f2d8a2102
NDThanh <ndthanh2306@gmail.com>
2025-08-07 12:58:28 +0700

    Frontend init

---
+++ frontend/src/App.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-red-600">✅ TailwindCSS hoạt động rồi!</h1>
+  </div>
+</template>
+
+<style scoped></style>
